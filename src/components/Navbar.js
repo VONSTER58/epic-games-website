9a166286ea91e58ba715609edb5d8f7f6db9f8e5
Von Bruma <vrbruma@gmail.com>
2022-08-02 21:03:53 +0800

    Added Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// CSS
+
+import '../styles/components/Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar_container'>This is a navbar.</div>
+  )
+}
+
+export default Navbar
