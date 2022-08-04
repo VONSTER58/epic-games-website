e0d5f2e6e6f29d601e0f645f7720bbcf0040af37
Von Bruma <vrbruma@gmail.com>
2022-08-04 21:23:57 +0800

    added logo, logo container and enhancements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,25 @@ import React from 'react';
 
 import '../styles/components/Navbar.css'
 
+// Images
+
+import EpicGamesLogo from "../assets/images/epic-games-logo.png"
+
 const Navbar = () => {
   return (
-    <div className='navbar_container'>This is a navbar.</div>
+    <div className='navbar_container'>
+      <div className='logo_container'>
+      <img className='logo' src={EpicGamesLogo}/></div>
+        <ul className='navbar_list'>
+        <li>STORE</li>
+        <li>NEWS</li>
+        <li>FAQ</li>
+        <li>HELP</li>
+        <li>UNREAL ENGINE</li>
+          <div className='divider'></div>
+        <li> ABOUT EPIC</li>
+  
+        </ul></div>
   )
 }
 
