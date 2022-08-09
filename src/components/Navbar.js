9f5cadfea9711ced83e352b169d396d86f529334
Von Bruma <vrbruma@gmail.com>
2022-08-09 21:06:33 +0800

    navbar enhancement, uploaded images on assets

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React from "react";
 
 // CSS
 
-import '../styles/components/Navbar.css'
+import "../styles/components/Navbar.css";
 
 // Images
 
-import EpicGamesLogo from "../assets/images/epic-games-logo.png"
+import EpicGamesLogo from "../assets/images/epic-games-logo.png";
 
 const Navbar = () => {
   return (
-    <div className='navbar_container'>
-      <div className='logo_container'>
-      <img className='logo' src={EpicGamesLogo}/></div>
-        <ul className='navbar_list'>
+    <div className="navbar_container">
+      <div className="logo_container">
+        <img className="logo" src={EpicGamesLogo} />
+      </div>
+      <ul className="navbar_list">
         <li>STORE</li>
         <li>NEWS</li>
         <li>FAQ</li>
         <li>HELP</li>
         <li>UNREAL ENGINE</li>
-          <div className='divider'></div>
+        <div className="divider"></div>
         <li> ABOUT EPIC</li>
-  
-        </ul></div>
-  )
-}
+      </ul>
+      <div className="sign-in">
+        <li>SIGN IN</li>
+      </div>
+      <div className="get-epic-games">
+        <li>GET EPIC GAMES</li>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
